8522935116a1732b850a3dec6804b4435c523a01
pavannaik19 <naik2006pavan@gmail.com>
2025-01-29 10:26:56 +0530

    Initial commit

---
+++ storybook/src/components/features.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Features from "./features";
+
+export default {
+  title: "Components/features",
+  component: Features,
+};
+
+export const Default = () => <Features />;
