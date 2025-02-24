8d24da0232fda2123a707de8871771d90f5d0c37
pavnnaik19 <naik2006pavan@gmail.com>
2025-02-24 23:58:44 +0530

    updating

---
+++ storybook/src/components/acecoinpay/acp.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Homepage from "./home";
+
+export default {
+  title: "components/acp/home",
+  component: Homepage,
+};
+
+export const Default = () => <Homepage/>;
