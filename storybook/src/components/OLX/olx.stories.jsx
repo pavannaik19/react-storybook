646cbedb8d86617d6ac3c8636e26241c6fa03da6
pavnnaik19 <naik2006pavan@gmail.com>
2025-03-04 00:27:42 +0530

    updating

---
+++ storybook/src/components/OLX/olx.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Olxhome } from "./olx";
+
+export default{
+    title:'Component/OlX/Olx Page',
+    Component:Olxhome,
+}
+
+export const Default =()=><Olxhome/> 
